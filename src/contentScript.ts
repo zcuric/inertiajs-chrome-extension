d44f8535f1b3b32650d743dcef134a92dc9e901b
Zdravko Curic <zcuric@gmail.com>
2025-07-09 23:43:46 +0200

    fix: Prevent content script from running in invalid context

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,12 +1,14 @@
 const SOURCE = "inertia-devtools-internal";
 
-const script = document.createElement("script");
-script.src = chrome.runtime.getURL("injected.js");
-(document.head || document.documentElement).appendChild(script);
-script.onload = () => script.remove();
+if (chrome.runtime?.id) {
+	const script = document.createElement("script");
+	script.src = chrome.runtime.getURL("injected.js");
+	(document.head || document.documentElement).appendChild(script);
+	script.onload = () => script.remove();
 
-window.addEventListener("message", (event) => {
-	if (event.source === window && event.data.source === SOURCE) {
-		chrome.runtime.sendMessage(event.data.message);
-	}
-});
+	window.addEventListener("message", (event) => {
+		if (event.source === window && event.data.source === SOURCE) {
+			chrome.runtime.sendMessage(event.data.message);
+		}
+	});
+}
