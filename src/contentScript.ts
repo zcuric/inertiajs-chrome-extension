1ebfdf660beac5496372a79ec76b9b7fed72d92c
Zdravko Curic <zcuric@gmail.com>
2025-07-09 23:45:18 +0200

    fix: Add error handling to content script

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,14 +1,24 @@
 const SOURCE = "inertia-devtools-internal";
 
 if (chrome.runtime?.id) {
-	const script = document.createElement("script");
-	script.src = chrome.runtime.getURL("injected.js");
-	(document.head || document.documentElement).appendChild(script);
-	script.onload = () => script.remove();
+	try {
+		const script = document.createElement("script");
+		script.src = chrome.runtime.getURL("injected.js");
+		(document.head || document.documentElement).appendChild(script);
+		script.onload = () => script.remove();
 
-	window.addEventListener("message", (event) => {
-		if (event.source === window && event.data.source === SOURCE) {
-			chrome.runtime.sendMessage(event.data.message);
-		}
-	});
+		window.addEventListener("message", (event) => {
+			if (
+				event.source === window &&
+				event.data.source === SOURCE &&
+				chrome.runtime?.id
+			) {
+				chrome.runtime.sendMessage(event.data.message);
+			}
+		});
+	} catch (e) {
+		// The extension context can become invalid if the extension is reloaded.
+		// We don't need to do anything here, the new content script will be injected
+		// on page reload.
+	}
 }
